46573a957a93badf4b7ec79e960a59ba72c2cf31
wrkmc cimancis <adam.vss23@gmail.com>
2024-07-23 14:42:48 +0700

    Update lines

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-// TODO: Uncomment baris kode dibawah ini untuk meng-import Halaman Hello ke dalam router
 import Hello from "./routes/hello";
 import Home from "./routes/home";
 
@@ -14,7 +13,6 @@ const router = createBrowserRouter([
     element: <Home />,
   },
 
-  // TODO: Uncomment baris kode dibawah ini untuk menambahkan routing baru ke Halaman Hello
   {
     path: "/hello",
     element: <Hello />,
@@ -28,7 +26,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

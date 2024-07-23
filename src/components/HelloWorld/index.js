defa7399d38cdf3c11a98321fb08551fafcec448
wrkmc cimancis <adam.vss23@gmail.com>
2024-07-23 14:40:51 +0700

    Update path

--- src/components/HelloWorld/index.js
+++ src/components/HelloWorld/index.js
@@ -1,14 +1,11 @@
-// TODO: Uncomment baris kode di bawah ini untuk meng-import gambar logo devcode yang berada di dalam folder assets
 import devcodeLogo from "../../assets/devcode-logo.png";
 import "./style.css";
 
 export const HelloWorld = () => {
   return (
     <div className="hello">
-      {/* TODO: Tambahkan gambar logo devcode yang sudah diimport sebelumnya ke dalam properti src pada elemen img di bawah ini */}
       <img data-cy="devcode-logo" src={devcodeLogo} alt="Devcode Logo" />
       <div data-cy="devcode-title">
-        {/* TODO: Tambahkan elemen heading 1 di bawah ini dengan berisi kalimat I'm ready for the next challenge! */}
         <h1>I'm ready for the next challenge!</h1>
         <h2>Hello World</h2>
       </div>
